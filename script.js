54f54784f3be4ab88e32ec29ce8d90fe3ed8ec07
Ranu <88536226+Ranu2211@users.noreply.github.com>
2024-12-22 20:54:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  arr.sort((a,b) => a-b);
+	let totalcost  = 0;
+	while(arr.length>1){
+		const first = arr.shift();
+		const second = arr.shift();
+
+		let cost  = first + second;
+		totalcost+= cost;
+		arr.push(cost);
+		arr.sort((a,b) => a-b);
+	}
+	return totalcost;
 }
 
 module.exports=mincost;
